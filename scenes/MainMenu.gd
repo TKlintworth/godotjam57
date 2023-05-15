61837367dcc23beba37169ddf0595d7aaab40c68
Tristan Klintworth <tristan.klintworth@centauricorp.com>
2023-05-15 19:27:21 -0400

    setting up scenes

---
+++ scenes/MainMenu.gd
@@ -0,0 +1,14 @@
+extends CanvasLayer
+
+@onready var main: Control = $Main
+
+
+
+
+
+func _on_play_button_pressed():
+	print("pressed play")
+
+
+func _on_exit_button_pressed():
+	print("pressed exit")
